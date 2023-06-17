9b06d2f101f07d946ed175e496fce96698dd8f00
Emre Terzioglu <50607143+EmreTech@users.noreply.github.com>
2023-06-17 16:37:36 -0700

    feat: add ratelimiting

--- thing/http/client.py
+++ thing/http/client.py
@@ -1,14 +1,15 @@
 from __future__ import annotations
 
 import aiohttp
-import urllib.parse as urlparse
+import asyncio
 import typing as t
 
 from .auth import Auth
+from .ratelimit import Bucket, GlobalBucket, BucketMigrated
+from .route import Route
 
 __all__ = ("Route", "HTTPClient",)
 
-HTTP_METHODS: t.Literal["GET", "POST", "PATCH", "PUT", "DELETE"]
 BASE_API_URL = "https://discord.com/api/v{0}"
 API_VERSION = 10
 
@@ -18,22 +19,22 @@ def _get_user_agent():
 def _get_base_url():
     return BASE_API_URL.format(API_VERSION)
 
-class Route:
-    def __init__(self, method: HTTP_METHODS, url: str, **params: t.Any):
-        self.method: HTTP_METHODS = method
-        self.url: str = url
-        self._orig_params: dict[str, t.Any] = params
-        self.params: dict[str, str] = {k: urlparse.quote(str(v)) for k, v in self._orig_params}
-
-    @property
-    def formatted_url(self):
-        return self.url.format_map(self.params)
-
 class HTTPClient:
-    def __init__(self, default_auth: Auth):
+    def __init__(self, default_auth: Auth, *, bucket_lag: float = 0.2, compare_reset_afters: bool = False):
         self._http: t.Optional[aiohttp.ClientSession] = None
         self._default_headers: dict[str, str] = {"User-Agent": _get_user_agent(), "Authorization": default_auth.header}
         self._base_url = _get_base_url()
+        self._default_bucket_lag = bucket_lag
+        self._default_compare_reset_afters = compare_reset_afters
+        # (local bucket, discord bucket) -> Bucket
+        self._buckets: dict[tuple[str, t.Optional[str]], Bucket] = {}
+        self._global_bucket: GlobalBucket = GlobalBucket(bucket_lag)
+
+    async def __aenter__(self):
+        return self
+    
+    async def __aexit__(self, *_):
+        await self.close()
 
     @property
     def http(self):
@@ -48,11 +49,14 @@ class HTTPClient:
 
         await self._http.close()
 
-    async def __aenter__(self):
-        return self
-    
-    async def __aexit__(self, *_):
-        await self.close()
+    def _get_bucket(self, key: tuple[str, t.Optional[str]]):
+        bucket = self._buckets.get(key)
+
+        if not bucket:
+            bucket = Bucket(self._default_bucket_lag, self._default_compare_reset_afters)
+            self._buckets[key] = bucket
+
+        return bucket
 
     async def request(
         self,
@@ -73,10 +77,46 @@ class HTTPClient:
         if headers:
             params["headers"] = headers
 
-        async with self.http.request(
-            route.method, 
-            self._base_url + route.formatted_url, 
-            **params
-        ) as resp:
-            content = await resp.text()
-            return content, resp.content_type
+        local_bucket = route.bucket
+        discord_bucket: t.Optional[str] = None
+        bucket = self._get_bucket((local_bucket, discord_bucket))
+
+        MAX_RETRIES = 5
+
+        for try_ in range(MAX_RETRIES):
+            try:
+                async with self._global_bucket:
+                    # log debug "The global bucket has been acquired."
+                    async with bucket:
+                        # log debug "The local bucket has been acquired."
+                        async with self.http.request(
+                            route.method, 
+                            self._base_url + route.formatted_url, 
+                            **params
+                        ) as resp:
+                            bucket.update_from(resp)
+                            await bucket.acquire()
+
+                            if 300 > resp.status >= 200:
+                                if resp.status == 204:
+                                    return (None, "")
+
+                                content = await resp.text()
+                                return (content, resp.content_type)
+                            
+                            if 500 > resp.status >= 400:
+                                if resp.status == 429:
+                                    continue
+
+                                raise Exception(await resp.text())
+                            
+                            if 600 > resp.status >= 500:
+                                if resp.status in (500, 502):
+                                    await asyncio.sleep(2 * try_ + 1)
+                                raise Exception(resp.status)
+            except BucketMigrated as e:
+                # log debug "Our bucket {e.old} has migrated to {e.new}."
+                bucket = self._get_bucket((local_bucket, e.new))
+
+        # log error "Tried to make request to {route.formatted_url} with method {route.method} {MAX_RETRIES} times."
+        return (None, "")
---
+++ thing/http/ratelimit.py
@@ -0,0 +1,130 @@
+import aiohttp
+import asyncio
+import datetime
+import typing as t
+
+__all__ = ("BucketMigrated", "Bucket", "GlobalBucket",)
+
+class BaseBucket:
+    def __init__(self, lag: float = 0.2):
+        self.lag: float = lag
+
+        self.bucket: str = ""
+        self.reset_after: float = 0.0
+        self._lock: asyncio.Event = asyncio.Event()
+        self._lock.set()
+
+    def update_from(self, response: aiohttp.ClientResponse):
+        pass
+
+    def lock_for(self, time: float):
+        if not self._lock.is_set():
+            return
+
+        print(f"Bucket {self.bucket} will be locked for {time} seconds.")
+        self._lock.clear()
+        asyncio.create_task(self._unlock(time))
+
+    async def _unlock(self, time: float):
+        await asyncio.sleep(time)
+        print(f"Bucket {self.bucket} will be unlocked.")
+        self._lock.set()
+
+    async def acquire(self):
+        await self._lock.wait()
+        # log debug "Bucket {self.bucket} has been acquired!"
+
+    async def __aenter__(self):
+        await self.acquire()
+        return self
+    
+    async def __aexit__(self, *_):
+        pass
+
+class BucketMigrated(Exception):
+    def __init__(self, old: str, new: str):
+        self.old: str = old
+        self.new: str = new
+        super().__init__(f"Bucket {old} has migrated to {new}.")
+
+class Bucket(BaseBucket):
+    def __init__(self, lag: float = 0.2, compare_reset_afters: bool = False):
+        super().__init__(lag)
+
+        self.compare_reset_afters: bool = compare_reset_afters
+
+        self.limit: int = 1
+        self.remaining: int = 1
+        self.reset: t.Optional[datetime.datetime] = None
+        self.bucket: str = ""
+        self.enabled: bool = True
+
+    def update_from(self, response: aiohttp.ClientResponse):
+        headers = response.headers
+
+        x_bucket: t.Optional[str] = headers.get("X-RateLimit-Bucket")
+        if x_bucket is None:
+            # log debug "Ratelimiting is not supported for this bucket."
+            self.enabled = False
+            return
+        
+        if self.bucket == "":
+            self.bucket = x_bucket
+        elif self.bucket != x_bucket:
+            self.migrate_to(x_bucket)
+
+        # from here on, the route has ratelimits
+
+        if headers.get("X-RateLimit-Global", False):
+            # log debug "This ratelimit is globally applied."
+            return
+        
+        x_limit: int = int(headers["X-RateLimit-Limit"])
+        if x_limit != self.limit:
+            self.limit = x_limit
+
+        x_remaining: int = int(headers.get("X-RateLimit-Remaining", 1))
+        if x_remaining < self.remaining:
+            self.remaining = x_remaining
+
+        x_reset: datetime.datetime = datetime.datetime.fromtimestamp(float(headers["X-RateLimit-Reset"]))
+        if x_reset != self.reset:
+            self.reset = x_reset
+
+        x_reset_after: float = float(headers["X-RateLimit-Reset-After"])
+        if x_reset_after != self.reset_after:
+            if self.compare_reset_afters:
+                current_timestamp = datetime.datetime.now(tz=datetime.timezone.utc).timestamp()
+                calculated_reset_after = current_timestamp - self.reset.timestamp()
+                if calculated_reset_after > 0:
+                    self.reset_after = min(calculated_reset_after, x_reset_after)
+            else:
+                self.reset_after = x_reset_after
+
+            self.reset_after += self.lag
+
+    def migrate_to(self, new: str):
+        self.enabled = False
+        raise BucketMigrated(self.bucket, new)
+
+    async def acquire(self):
+        if self.remaining == 0:
+            # log debug "Bucket {self.bucket} will be auto-locked."
+            self.lock_for(self.reset_after)
+
+        await super().acquire()
+
+class GlobalBucket(BaseBucket):
+    def __init__(self, lag: float = 0.2):
+        super().__init__(lag)
+
+        self.bucket = "GLOBAL"
+
+    def update_from(self, response: aiohttp.ClientResponse):
+        headers = response.headers
+
+        if not headers.get("X-RateLimit-Global", False):
+            # log debug "This ratelimit is not globally applied."
+            return
+
+        self.reset_after: float = float(headers["Retry-After"]) + self.lag
