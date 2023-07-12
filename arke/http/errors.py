91bd0954f35851c64ecc04d4f4c93272705197e7
Emre Terzioglu <50607143+EmreTech@users.noreply.github.com>
2023-07-11 22:14:48 -0700

    fix: typing problems

--- arke/http/errors.py
+++ arke/http/errors.py
@@ -1,5 +1,7 @@
 import typing as t
 
+from ..internal.json import JSONObject
+
 __all__ = (
     "HTTPException",
     "Unauthorized",
@@ -16,13 +18,13 @@ def _flatten_error_dict(d: dict[str, t.Any], *, parent: str = ""):
 
         if isinstance(v, list) and k == "_errors":
             ret[parent] = "\n".join([v2["message"] for v2 in v])
-        else:
+        elif isinstance(v, dict):
             ret.update(_flatten_error_dict(v, parent=full_key))
 
     return ret
 
 class HTTPException(Exception):
-    def __init__(self, original: str | t.Optional[dict[str, t.Any]], status: int, status_msg: str):
+    def __init__(self, original: str | t.Optional[t.Mapping[str, t.Any]], status: int, status_msg: t.Optional[str]):
         code: int = 0
         text: str
 
@@ -38,9 +40,14 @@ class HTTPException(Exception):
             else:
                 text = message
         else:
+            if t.TYPE_CHECKING:
+                original = t.cast(str | None, original)
+
             text = original or ""
 
-        fmt = f"{status} {status_msg}"
+        fmt = f"{status}"
+        if status_msg:
+            fmt += f" {status_msg}"
         if code:
             fmt += f" (discord code: {code})"
         if text:
@@ -49,15 +56,15 @@ class HTTPException(Exception):
         super().__init__(fmt)
 
 class Unauthorized(HTTPException):
-    def __init__(self, original: str | t.Optional[dict[str, t.Any]]):
+    def __init__(self, original: str | t.Optional[t.Mapping[str, t.Any]]):
         super().__init__(original, 401, "Unauthorized")
 
 class Forbidden(HTTPException):
-    def __init__(self, original: str | t.Optional[dict[str, t.Any]]):
+    def __init__(self, original: str | t.Optional[t.Mapping[str, t.Any]]):
         super().__init__(original, 403, "Forbidden")
 
 class NotFound(HTTPException):
-    def __init__(self, original: str | t.Optional[dict[str, t.Any]]):
+    def __init__(self, original: str | t.Optional[t.Mapping[str, t.Any]]):
         super().__init__(original, 404, "Not Found")
 
 class ServerError(HTTPException):
