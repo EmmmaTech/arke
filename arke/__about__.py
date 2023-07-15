01dd14dc3ef77c1a6490e1dc5b32df52884dd289
Emre Terzioglu <50607143+EmreTech@users.noreply.github.com>
2023-07-15 14:56:47 -0700

    chore: make versioning dynamic

---
+++ arke/__about__.py
@@ -0,0 +1,48 @@
+# SPDX-License-Identifier: MIT
+
+__name__ = "arke"
+__description__ = "Python Discord API Wrapper made from the ground up."
+__version__ = "0.1.0"
+
+import typing as t
+
+class VersionInfo(t.NamedTuple):
+    major: int
+    minor: int
+    patch: int
+    releaseinfo: t.Literal["alpha", "beta", "candidate", "stable"] = "stable"
+    serial: t.Optional[int] = None
+
+    @classmethod
+    def from_string(cls, raw: str, /):
+        serial = None
+        releaseinfo = "stable"
+        if "-" in raw:
+            version, releaseinfo = raw.split("-")
+
+            if "a" in releaseinfo:
+                serial = int(releaseinfo[1:])
+                releaseinfo = "alpha"
+            elif "b" in releaseinfo:
+                serial = int(releaseinfo[1:])
+                releaseinfo = "beta" 
+            else:
+                serial = int(releaseinfo[2:])
+                releaseinfo = "candidate"
+        else:
+            version = raw
+
+        major, minor, patch = version.split(".")
+
+        return cls(
+            major=int(major), 
+            minor=int(minor), 
+            patch=int(patch), 
+            releaseinfo=releaseinfo, 
+            serial=serial
+        )
+    
+
+version_info: VersionInfo = VersionInfo.from_string(__version__)
+
+del VersionInfo, t
